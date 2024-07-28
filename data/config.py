cfac7d80130e13ac8674a36907ed23fad3cf0891
Axcent <97802272+Axcent-ape@users.noreply.github.com>
2024-07-28 13:14:55 +0300

    Add files via upload

---
+++ data/config.py
@@ -0,0 +1,39 @@
+# api id, hash
+API_ID = 1488
+API_HASH = 'abcde1488'
+
+
+DELAYS = {
+    'ACCOUNT': [5, 15],  # delay between connections to accounts (the more accounts, the longer the delay)
+    'GAME': [5, 10],     # delay after the game
+    'REPEAT': [5, 18]
+}
+
+# what games the soft will play (in case of several variants it is selected randomly)
+GAMES = {
+    "512": True,
+    "stack": True
+}
+
+PROXY = {
+    "USE_PROXY_FROM_FILE": False,  # True - if use proxy from file, False - if use proxy from accounts.json
+    "PROXY_PATH": "data/proxy.txt",  # path to file proxy
+    "TYPE": {
+        "TG": "socks5",  # proxy type for tg client. "socks4", "socks5" and "http" are supported
+        "REQUESTS": "socks5"  # proxy type for requests. "http" for https and http proxys, "socks5" for socks5 proxy.
+        }
+}
+# session folder (do not change)
+WORKDIR = "sessions/"
+
+
+# timeout in seconds for checking accounts on valid
+TIMEOUT = 30
+
+SOFT_INFO = f"""{"Clayton Game".center(40)}
+Soft for https://t.me/claytoncoinbot; play in 512 game;
+play in stack game, complete tasks; register accounts in web app
+
+The soft also collects statistics on accounts and uses proxies from {f"the {PROXY['PROXY_PATH']} file" if PROXY['USE_PROXY_FROM_FILE'] else "the accounts.json file"}
+To buy this soft with the option to set your referral link write me: https://t.me/Axcent_ape
+"""
